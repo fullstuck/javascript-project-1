8988d1c40eeef14033c2d034a8f8cbd9d8d8395a
M. Alif Raher <alifraher@gmail.com>
2018-07-20 15:48:21 +0700

    Add Level 6

--- 2-manipulation-string/index.js
+++ 2-manipulation-string/index.js
@@ -9,3 +9,9 @@ const sentenceC = sentenceA + " " + sentenceB;
 const sentenceD = `${sentenceA} ${sentenceB}`;
 console.log(sentenceC);
 console.log(sentenceD);
+
+console.log("Level 6");
+console.log(sentenceA.toUpperCase());
+console.log(sentenceB.toUpperCase());
+console.log(sentenceA.toLowerCase());
+console.log(sentenceB.toLowerCase());
