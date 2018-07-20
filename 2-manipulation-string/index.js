39b0e701992d44ad9021f566aad6a0bf08d48447
M. Alif Raher <alifraher@gmail.com>
2018-07-20 15:34:32 +0700

    Add Level 4

---
+++ 2-manipulation-string/index.js
@@ -0,0 +1,7 @@
+console.log("Level 4");
+const sentenceA = "Hello World!";
+const sentenceB = "Good Bye!";
+const sentenceC = "testing"
+console.log(sentenceA);
+console.log(sentenceB);
+console.log(sentenceC);
