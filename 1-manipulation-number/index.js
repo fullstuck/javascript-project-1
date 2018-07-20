1d67ddf3e5ba98a1e1a41818595480acb556f1c6
M. Alif Raher <alifraher@gmail.com>
2018-07-20 14:44:33 +0700

    Add Level 1

--- 1-manipulation-number/index.js
+++ 1-manipulation-number/index.js
@@ -5,3 +5,10 @@ const numberC = 0.02;
 console.log(numberA);
 console.log(numberB);
 console.log(numberC);
+
+console.log("Level 1");
+console.log(numberA + numberB);
+console.log(numberA - numberC);
+console.log(numberB * numberC);
+console.log(numberA / numberC);
+console.log(numberA % numberB);
