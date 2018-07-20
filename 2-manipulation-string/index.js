293d49c7d8232007dfe4172329bdb34c37e7623a
M. Alif Raher <alifraher@gmail.com>
2018-07-20 16:00:14 +0700

    Add Level 7

--- 2-manipulation-string/index.js
+++ 2-manipulation-string/index.js
@@ -15,3 +15,16 @@ console.log(sentenceA.toUpperCase());
 console.log(sentenceB.toUpperCase());
 console.log(sentenceA.toLowerCase());
 console.log(sentenceB.toLowerCase());
+
+console.log("Level 7");
+const sentence = "hello everyone";
+
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+console.log(toTitleCase(sentence));
