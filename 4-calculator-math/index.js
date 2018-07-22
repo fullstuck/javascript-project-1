c97606f935e26739ba3f19b8b36953755d9d95c4
M. Alif Raher <alifraher@gmail.com>
2018-07-22 22:43:52 +0700

    Add Level 15

--- 4-calculator-math/index.js
+++ 4-calculator-math/index.js
@@ -7,3 +7,31 @@ console.log(numberA - numberB);
 console.log(numberA * numberB);
 console.log(numberA / numberB);
 console.log(numberA % numberB);
+
+console.log("Level 15");
+
+function add(param1, param2) {
+    return param1 + param2;
+}
+
+function substract(param1, param2) {
+    return param1 - param2;
+}
+
+function multiply(param1, param2) {
+    return param1 * param2;
+}
+
+function divide(param1, param2) {
+    return param1 / param2;
+}
+
+function modulo(param1, param2) {
+    return param1 % param2;
+}
+
+console.log(add(numberA, numberB));
+console.log(substract(numberA, numberB));
+console.log(multiply(numberA, numberB));
+console.log(divide(numberA, numberB));
+console.log(modulo(numberA, numberB));
