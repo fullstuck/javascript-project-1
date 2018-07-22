8d68b4795168cc00aae30ef8231269ba3be635dd
M. Alif Raher <alifraher@gmail.com>
2018-07-22 20:32:40 +0700

    Add Level 10

--- 3-comparison-number/index.js
+++ 3-comparison-number/index.js
@@ -1,6 +1,6 @@
 console.log("Level 8");
 
-let numberA = 3;
+let numberA = 5;
 let numberB = 4;
 if (numberA > numberB) {
     console.log(`${numberA} are bigger than ${numberB}`);
@@ -9,6 +9,7 @@ if (numberA > numberB) {
 }
 
 console.log("Level 9");
+
 if (numberA > numberB) {
     console.log(`${numberA} are bigger than ${numberB}`);
 } else if (numberA < numberB) {
@@ -16,3 +17,7 @@ if (numberA > numberB) {
 } else {
     console.log(`${numberA} are as same as ${numberB}`)
 }
+
+console.log("Level 10");
+
+if (numberA > numberB ? console.log(`${numberA} are bigger than ${numberB}`) : console.log(`${numberA} are smaller than ${numberB}`));
