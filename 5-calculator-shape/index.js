c2d0fce81977659b0d68b6e0ed354eee1d2dabad
M. Alif Raher <alifraher@gmail.com>
2018-07-23 00:50:21 +0700

    Add Level 18

--- 5-calculator-shape/index.js
+++ 5-calculator-shape/index.js
@@ -27,3 +27,56 @@ console.log(`Cube Area = ${cubeArea}`);
 console.log(`Cube Volume = ${cubeVolume}`);
 console.log(`Tube Area = ${tubeArea}`);
 console.log(`Tube Volume = ${tubeVolume}`);
+
+console.log("Level 18");
+
+function squareAreaFunc(side) {
+    return side * side;
+}
+
+function squarePerimeterFunc(side) {
+    return side * 4;
+}
+
+function rectangleAreaFunc(length, width) {
+    return length * width;
+}
+
+function rectanglePerimeterFunc(length, width) {
+    return (length * 2) + (width * 2);
+}
+
+function circleAreaFunc(phi, radius) {
+    return phi * (radius * radius);
+}
+
+function circleCircumferenceFunc(phi, radius) {
+    return 2 * phi * radius;
+}
+
+function cubeAreaFunc(side) {
+    return 6 * (length * length);
+}
+
+function cubeVolumeFunc(side) {
+    return length * length * length;
+}
+
+function tubeAreaFunc(phi, radius, height) {
+    return 2 * phi * radius * (height + radius);
+}
+
+function tubeVolumeFunc(phi, radius, height) {
+    return phi * (radius * radius) * height;
+}
+
+console.log(`Square Area = ${squareAreaFunc(length)}`);
+console.log(`Square Perimeter = ${squarePerimeterFunc(length)}`);
+console.log(`Rectangle Area = ${rectangleAreaFunc(length, width)}`);
+console.log(`Rectangle Perimeter = ${rectanglePerimeterFunc(length, width)}`);
+console.log(`Circle Area = ${circleAreaFunc(phi, radius)}`);
+console.log(`Circle Circumference = ${circleCircumferenceFunc(phi, radius)}`);
+console.log(`Cube Area = ${cubeAreaFunc(length)}`);
+console.log(`Cube Volume = ${cubeVolumeFunc(length)}`);
+console.log(`Tube Area = ${tubeAreaFunc(phi, radius, height)}`);
+console.log(`Tube Volume = ${tubeVolumeFunc(phi, radius, height)}`);
