be9712a475da7f309cf998c2e0ae4dfda8c6cc58
M. Alif Raher <alifraher@gmail.com>
2018-07-22 22:01:43 +0700

    Add Level 11

--- 3-comparison-number/index.js
+++ 3-comparison-number/index.js
@@ -1,23 +1,44 @@
 console.log("Level 8");
 
-let numberA = 5;
-let numberB = 4;
+let numberA = 4;
+let numberB = 6;
 if (numberA > numberB) {
-    console.log(`${numberA} are bigger than ${numberB}`);
+    console.log(`${numberA} is bigger than ${numberB}`);
 } else {
-    console.log(`${numberA} are smaller than ${numberB}`);
+    console.log(`${numberA} is smaller than ${numberB}`);
 }
 
 console.log("Level 9");
 
 if (numberA > numberB) {
-    console.log(`${numberA} are bigger than ${numberB}`);
+    console.log(`${numberA} is bigger than ${numberB}`);
 } else if (numberA < numberB) {
-    console.log(`${numberA} are smaller than ${numberB}`);
+    console.log(`${numberA} is smaller than ${numberB}`);
 } else {
-    console.log(`${numberA} are as same as ${numberB}`)
+    console.log(`${numberA} is as same as ${numberB}`)
 }
 
 console.log("Level 10");
 
-if (numberA > numberB ? console.log(`${numberA} are bigger than ${numberB}`) : console.log(`${numberA} are smaller than ${numberB}`));
+if (numberA > numberB ? console.log(`${numberA} is bigger than ${numberB}`) : console.log(`${numberA} is smaller than ${numberB}`));
+
+console.log("Level 11");
+
+let numberC = 5;
+if (numberC < numberA) {
+    console.log(`${numberC} is smaller than ${numberA}`);
+} else if (numberC == numberA) {
+    console.log(`${numberC} is as same as ${numberA}`);
+} else if (numberC > numberA && numberC < numberB) {
+    console.log(`${numberC} is bigger than ${numberA} and smaller than ${numberB}`);
+} else if (numberC == numberB) {
+    console.log(`${numberC} is as same as ${numberB}`);
+} else {
+    console.log(`${numberC} is bigger than ${numberB}`);
+}
+
+if (numberC > numberA || numberC == numberA) {
+    console.log(`${numberC} is not bigger than ${numberA}`);
+} else {
+    console.log(`${numberC} is bigger than ${numberA}`);
+}
