bfbbb466e6ae6700eaa7ca55f4ba15bd1b5aaeb8
M. Alif Raher <alifraher@gmail.com>
2018-07-22 22:37:47 +0700

    Add Level 14

---
+++ 4-calculator-math/index.js
@@ -0,0 +1,9 @@
+console.log("Level 14");
+
+let numberA = 10;
+let numberB = 5;
+console.log(numberA + numberB);
+console.log(numberA - numberB);
+console.log(numberA * numberB);
+console.log(numberA / numberB);
+console.log(numberA % numberB);
