6559f249b962336522298c1c10412a9bb2e6395b
M. Alif Raher <alifraher@gmail.com>
2018-07-22 20:27:34 +0700

    Add Level 8

---
+++ 3-comparison-number/index.js
@@ -0,0 +1,9 @@
+console.log("Level 8");
+
+let numberA = 3;
+let numberB = 4;
+if (numberA > numberB) {
+    console.log(`${numberA} are bigger than ${numberB}`);
+} else {
+    console.log(`${numberA} are smaller than ${numberB}`);
+}
