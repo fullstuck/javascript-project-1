d1ba0f236980748f56b603a1871f8bbf138de3f3
M. Alif Raher <alifraher@gmail.com>
2018-07-23 15:26:35 +0700

    Cleaning code

--- 5-calculator-shape/index.js
+++ 5-calculator-shape/index.js
@@ -1,30 +1,38 @@
 console.log("Level 17");
 
-let length = 20;
-let width = 10;
-let height = 15;
-let radius = 28;
-const phi = 3.14;
+const length = 20;
+const width = 10;
+const height = 15;
+const radius = 28;
+const phi = Math.PI;
 
 let squareArea = length * length;
 let squarePerimeter = length * 4;
+
 let rectangleArea = length * width;
 let rectanglePerimeter = (length * 2) + (width * 2);
+
 let circleArea = phi * (radius * radius);
 let circleCircumference = 2 * phi * radius;
+
 let cubeArea = 6 * (length * length);
 let cubeVolume = length * length * length;
+
 let tubeArea = 2 * phi * radius * (height + radius);
 let tubeVolume = phi * (radius * radius) * height;
 
 console.log(`Square Area = ${squareArea}`);
 console.log(`Square Perimeter = ${squarePerimeter}`);
+
 console.log(`Rectangle Area = ${rectangleArea}`);
 console.log(`Rectangle Perimeter = ${rectanglePerimeter}`);
+
 console.log(`Circle Area = ${circleArea}`);
 console.log(`Circle Circumference = ${circleCircumference}`);
+
 console.log(`Cube Area = ${cubeArea}`);
 console.log(`Cube Volume = ${cubeVolume}`);
+
 console.log(`Tube Area = ${tubeArea}`);
 console.log(`Tube Volume = ${tubeVolume}`);
 
@@ -72,12 +80,16 @@ function tubeVolumeFunc(phi, radius, height) {
 
 console.log(`Square Area = ${squareAreaFunc(length)}`);
 console.log(`Square Perimeter = ${squarePerimeterFunc(length)}`);
+
 console.log(`Rectangle Area = ${rectangleAreaFunc(length, width)}`);
 console.log(`Rectangle Perimeter = ${rectanglePerimeterFunc(length, width)}`);
+
 console.log(`Circle Area = ${circleAreaFunc(phi, radius)}`);
 console.log(`Circle Circumference = ${circleCircumferenceFunc(phi, radius)}`);
+
 console.log(`Cube Area = ${cubeAreaFunc(length)}`);
 console.log(`Cube Volume = ${cubeVolumeFunc(length)}`);
+
 console.log(`Tube Area = ${tubeAreaFunc(phi, radius, height)}`);
 console.log(`Tube Volume = ${tubeVolumeFunc(phi, radius, height)}`);
 
@@ -101,5 +113,6 @@ function calculateTubeVolumeFunc(circleAreaFunc, height) {
 
 console.log(`Cube Area = ${calculateCubeAreaFunc(squareAreaFunc(length, length))}`);
 console.log(`Cube Volume = ${calculateCubeVolumeFunc(length, squareAreaFunc(length, length))}`);
+
 console.log(`Tube Area = ${calculateTubeAreaFunc(circleCircumferenceFunc(phi, radius), height, radius)}`);
 console.log(`Tube Volume = ${calculateTubeVolumeFunc(circleAreaFunc(phi, radius), height)}`);
