55fc15c6bdd07d3dcb76d787d597060421e7375e
M. Alif Raher <alifraher@gmail.com>
2018-07-27 09:28:15 +0700

    Add Level 25

--- 7-address-book/index.js
+++ 7-address-book/index.js
@@ -1,3 +1,8 @@
 console.log("Level 24");
 
 console.log("Array = [name, email, address, phoneNumber]")
+
+console.log("Level 25");
+
+const names = ["john", "luky", "james", "dave", "robin"];
+console.log(names);
