8066e9574077afad61fa272266e3fd31220dce84
M. Alif Raher <alifraher@gmail.com>
2018-07-27 09:30:52 +0700

    Add Level 28

--- 8-todo-list/index.js
+++ 8-todo-list/index.js
@@ -1,3 +1,8 @@
 console.log("Level 27");
 
 console.log("Array = [name, task, place, date]")
+
+console.log("Level 28");
+
+const names = ["john", "luky", "james", "dave", "robin"];
+console.log(names);
